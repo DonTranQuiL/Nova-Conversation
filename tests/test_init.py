848df53670024e1c77aae270c213caa3e72b5b28
TranQuiL <90527254+malosaaa@users.noreply.github.com>
2026-05-15 20:49:14 +0200

    Update test_init.py
    silly me i forget to edit this hehe

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,8 +1,8 @@
 """Test the KNMI Seismisch integration."""
 
-from custom_components.knmi_seismisch.const import DOMAIN
+from custom_components.nova_conversation.const import DOMAIN
 
 
 async def test_domain_name():
     """A simple test to ensure pytest is running correctly."""
-    assert DOMAIN == "knmi_seismisch"
+    assert DOMAIN == "nova_conversation"
